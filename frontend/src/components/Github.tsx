ed1088588aa7395e81c1593866db2c6b7b977912
bruzz-bruzz <frkhor@gmail.com>
2026-08-15 16:16:13 +0800

    added slight styling for tos and privacy policy in frontend

--- frontend/src/components/Github.tsx
+++ frontend/src/components/Github.tsx
@@ -8,6 +8,8 @@ export default function Github({repo}:Data){
             <h1>Made by bruzz-bruzz</h1>
             <h3><a href='https://github.com/bruzz-bruzz' className='underline' target="_blank">GitHub Profile</a></h3>
             <h4><a href={`${repo}`} className='underline' target="_blank">GitHub Repository</a></h4>
+            <h3><a className='underline' href="https://github.com/bruzz-bruzz/coding-contest-tracker/blob/main/PRIVACY.md">Privacy Policy</a></h3>
+            <h3><a className='underline' href="https://github.com/bruzz-bruzz/coding-contest-tracker/blob/main/TOS.md">Terms of Service</a></h3>
         </div>
     )
 }
